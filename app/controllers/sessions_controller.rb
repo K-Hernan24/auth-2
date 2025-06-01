540ae08d578cc580983ea88f9e5ac22c342820fc
Hernan Mendoza <hernan.mendoza@kellogg.northwestern.edu>
2025-06-01 08:02:21 +0000

    ReferenceDoc_8_PartI&II

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,19 @@ class SessionsController < ApplicationController
     # 2. if the user exists -> check if they know their password
     # 3. if they know their password -> login is successful
     # 4. if the user doesn't exist or they don't know their password -> login fails
-    flash["notice"] = "Welcome."
-    redirect_to "/companies"
+    @user = User.find_by({"email"=>params["email"]})
+    if @user
+      if @user["password"] == params["password"]
+        flash["notice"] = "You have logged in"
+        redirect_to "/companies"
+      else
+        flash["notice"] = "Unsuccessful login"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Unsuccessful login"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
